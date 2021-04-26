a82b57e16739aa2be3738f061d158229c2ab854f
TkachLermontov <62636323+TkachLermontov@users.noreply.github.com>
2021-04-26 18:36:43 +0500

    main.py, все в data нерабочее

---
+++ code/print_timetable.py
@@ -0,0 +1,60 @@
+import argparse
+import csv
+
+
+def execute_from_database(file_name, ids=True, names=True, numbers=True, dates=True):
+    # Импорт библиотеки
+    import sqlite3
+
+    # Подключение к БД
+    con = sqlite3.connect("db/timetable.db")
+
+    # Создание курсора
+    cur = con.cursor()
+
+    # Выполнение запроса и получение всех результатов
+    result = cur.execute("""SELECT * FROM clients""").fetchall()
+
+    lst = list()
+
+    # Делаем массив со словарями с данными из базы данных
+    for cor in result:
+        dic = dict()
+        for i in range(len(cor)):
+            if i == 0 and ids:
+                dic['id'] = cor[i]
+            if i == 1 and names:
+                dic['name'] = cor[i]
+            if i == 2 and numbers:
+                dic['number'] = cor[i]
+            if i == 3 and dates:
+                dic['data'] = cor[i]
+        lst.append(dic)
+    return file_name[0], lst
+
+
+# Используем консоль для получения аргументов
+parser = argparse.ArgumentParser()
+parser.add_argument('file_name', type=str, nargs=1,
+                    help='name for css file, which will be generated in \'generated\' fold. '
+                         '!!! Should be written without file extension !!!')
+parser.add_argument('--no-id', action='store_const', const=False, default=True, dest='id',
+                    help='data\'ll be taken from database without clients\' id')
+parser.add_argument('--no-name', action='store_const', const=False, default=True, dest='name',
+                    help='data\'ll be taken from database without clients\' names')
+parser.add_argument('--no-number', action='store_const', const=False, default=True, dest='number',
+                    help='data\'ll be taken from database without clients\' numbers')
+parser.add_argument('--no-date', action='store_const', const=False, default=True, dest='date',
+                    help='data\'ll be taken from database without clients\' haircut dates')
+args = parser.parse_args()
+
+# при помощи
+file, data = execute_from_database(args.file_name, args.id, args.name, args.number, args.date)
+file = 'generated\\' + file + '.csv'
+with open(file, 'w', newline='') as f:
+    writer = csv.DictWriter(
+        f, fieldnames=list(data[0].keys()),
+        delimiter='-')
+    writer.writeheader()
+    for d in data:
+        writer.writerow(d)
